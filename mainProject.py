cf0d54f6adde160d98a8d5b85e71e80ced0f0b3a
DV155 <dan0024175@gmail.com>
2025-12-19 15:46:08 +0100

    Added breaks between salons, logic for sorting through the price page, this is the MVP

--- mainProject.py
+++ mainProject.py
@@ -1,4 +1,5 @@
 from urllib.request import urlopen
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
 #Code requires local file with the data
@@ -10,17 +11,31 @@ for line in fhand:
         html = urlopen(url).read()
         soup = BeautifulSoup(html, "html.parser")
         neededLinks = soup.find_all('a')
-        for link in neededLinks:
+        counter = 0
+        print(url)
+        for link in neededLinks: #Logic to find link to price pages, might be removed l8r
             Inhref = link.get('href')
             textL = link.text.lower()
-            keywords = ["preis", "pric", "dienst", "servic", "behandl"]
+            keywords = ["preis", "pric", "dienst", "servic", "behandl", "leist"]
             if any(keyword in textL for keyword in keywords):
-                priceUrl = Inhref
+                priceUrl = urljoin(url, Inhref)
                 priceHtml = urlopen(priceUrl).read()
                 priceSoup = BeautifulSoup(priceHtml, "html.parser")
+                neededPrice = priceSoup.find_all()
+                for counter, prices in enumerate(neededPrice): #Logic to get prices
+                    if "€" in prices.text:
+                        try:
+                            print(neededPrice[counter - 1].text)
+                            print(neededPrice[counter].text)
+                            print(neededPrice[counter + 1].text)
+                        except:
+                            continue
+                    else:
+                        continue
+                break
+                print("----------")
             else:
                 continue
-
     except:
         print("Invalid link")
         continue
